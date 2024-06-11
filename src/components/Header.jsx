bae94c6f3c88bd64d01cbf20ee18c9e7c1e6372a
Znilcosta <zenilson.costaf@hotmail.com>
2024-06-11 16:38:06 -0300

    primeiro commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-light">
+        <a>
+          <img />
+        </a>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
