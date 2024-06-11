342884d8cfb86a9cab4f276496208f43be442e68
Znilcosta <zenilson.costaf@hotmail.com>
2024-06-11 17:02:58 -0300

    Criando Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 
 function Header() {
   return (
-    <div>
-      <nav className="navbar navbar-light bg-light">
-        <a>
-          <img />
+    <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          Teste
         </a>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link">Processos</a>
+            </li>
+            <li>
+              <a>Processos</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
   );
