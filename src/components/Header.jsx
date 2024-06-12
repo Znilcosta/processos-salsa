643a948c6508335f9a6b598569aa720e19f639df
Znilcosta <zenilson.costaf@hotmail.com>
2024-06-12 09:50:14 -0300

    style Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,47 @@ import React from 'react';
 function Header() {
   return (
     <div className="container-fluid">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Teste
-        </a>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link">Processos</a>
-            </li>
-            <li>
-              <a>Processos</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <div className="row">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="col">
+            <a className="navbar-brand " style={{ marginLeft: 20 }} href="#">
+              LOGO
+            </a>
+          </div>
+          <div className="col-md-auto"></div>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Calendário
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Processos
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contatos
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Mais
+                </a>
+              </li>
+            </ul>
+          </div>
+          <button
+            type="button"
+            className="btn btn-outline-secondary btn-block"
+            style={{ marginRight: 20 }}
+          >
+            Entrar
+          </button>
+        </nav>
+      </div>
     </div>
   );
 }
