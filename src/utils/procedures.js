c2bc8b020a2108225ed1bb0d603b69518b6f5fce
Znilcosta <zenilson.costaf@hotmail.com>
2024-06-17 12:58:32 -0300

    Create procedure.js

---
+++ src/utils/procedures.js
@@ -0,0 +1,22 @@
+const procedures = [
+  {
+    imgFolder:
+      'https://static-00.iconduck.com/assets.00/folder-color-purple-icon-2048x1613-j8im97ws.png',
+    procedureTitle: 'Capacitação',
+    urlFolders: '',
+  },
+  {
+    imgFolder:
+      'https://static-00.iconduck.com/assets.00/folder-color-purple-icon-2048x1613-j8im97ws.png',
+    procedureTitle: 'SHOP9',
+    urlFolders: '',
+  },
+  {
+    imgFolder:
+      'https://static-00.iconduck.com/assets.00/folder-color-purple-icon-2048x1613-j8im97ws.png',
+    procedureTitle: 'Troca e Garantia',
+    urlFolders: '',
+  },
+];
+
+export default procedures;
